719c8dbcd8d146c3920f13cab544a8cbd46f34a4
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2023-06-03 18:48:37 +0500

    Create Backend

---
+++ app/api/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+    return path.join(process.cwd(), "data", "data.json");
+}
+
+function extractData(filePath) {
+    const jsonData = fs.readFileSync(filePath);
+    const data = JSON.parse(jsonData);
+    return data;
+}
+
+export async function POST(req) {
+    const { searchParams } = new URL(req.url);
+    const [email, comment] = searchParams.values();
+    const data = extractData(buildPath());
+
+    // CHECK FOR DUPLICATES
+    const duplicate = data.find((item) => item.email === email);
+    if (duplicate) {
+        return new Response(
+            JSON.stringify({
+                error: "Duplicate email",
+            }),
+            {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+    }
+    // ADD NEW ITEM
+    data.push({ email, comment });
+
+    fs.writeFileSync(buildPath(), JSON.stringify(data));
+    return new Response(
+        JSON.stringify({
+            status: "success",
+        }),
+        {
+            status: 200,
+            headers: {
+                "content-type": "application/json; charset=utf-8",
+            },
+        }
+    );
+
+    // return NextResponse.json({ comment, email });
+}
--- components/Sections/About.jsx
+++ components/Sections/About.jsx
@@ -1,7 +1,39 @@
 'use client'
 
-function About(params) {
-    return (<div className="flex-auto py-4 color-content flex flex-row flex-wrap gap-[6%] max-sm:flex-col max-sm:items-center max-sm:gap-2">
+import { useState, useRef } from "react";
+
+
+
+function About() {
+
+    const [btn, setBtn] = useState("Submit");
+    const [isDisabled, setDisabled] = useState(false);
+    const emailRef = useRef();
+    const commentRef = useRef();
+
+    async function handleSubmit(e) {
+
+        console.log("called");
+        setDisabled(true);
+        e.preventDefault();
+        const response = await fetch(
+            `/api?email=${emailRef.current.value}&comment=${commentRef.current.value}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+
+            }
+        );
+        const res = await response.json();
+        console.log(res);
+        if (res.status === "success") {
+            setBtn("Submitted");
+        }
+    }
+
+    return (<div className="flex-auto py-4 color-content flex flex-row flex-wrap gap-[6%] max-sm:flex-col  max-sm:gap-2">
         <div className="sm:w-[47%]">
             <h1 className="text-2xl font-semibold  text-white">About Us</h1>
             <p className=" text-gray-400 mt-4">Technology enthusiast and web developer. Passionate about coding and creating magical digital experiences. Let's create something awesome together!</p>
@@ -9,16 +41,28 @@ function About(params) {
 
         </div>
         <div className="sm:w-[47%]">
-            <h1 className="text-2xl font-semibold  text-white">Contact Us</h1>
-            <label for="input-group-1" class="block mb-2 text-sm font-medium  text-white text-left">Your Email</label>
-            <div class="relative mb-6">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5  text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+            <form onSubmit={handleSubmit}>
+                <h1 className="text-2xl font-semibold  text-white">Contact Us</h1>
+                <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium  text-white text-left">Your Email</label>
+                <div className="relative mb-6">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" className="w-5 h-5  text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                    </div>
+                    <input ref={emailRef} type="text" id="input-group-1" className=" border  text-sm rounded-lg  block w-full pl-10 p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="some@place.com" required />
                 </div>
-                <input type="text" id="input-group-1" class=" border  text-sm rounded-lg  block w-full pl-10 p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@flowbite.com" />
-            </div>
-            <label for="message" class="block mb-2 text-sm font-medium  text-white text-left">Your message</label>
-            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm  rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                <div className="mb-6">
+                    <label htmlFor="message" className="block mb-2 text-sm font-medium  text-white text-left">Your message</label>
+                    <textarea ref={commentRef} id="message" rows="4" className="block p-2.5 w-full text-sm  rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..." required></textarea>
+                </div>
+                <div className="text-left mb-6">
+                    <button
+                        type="submit"
+                        className={`left-0 focus:ring-4 focus:outline-none  rounded-lg border  text-sm font-medium px-5 py-2.5  focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600 disabled:bg-gray-900 disabled:cursor-not-allowed`}
+                        disabled={isDisabled}
+                    >{btn}
+                    </button>
+                </div>
+            </form>
         </div>
     </div>)
 }
