20b2f8c002746626a81303837e4d1f71539e2363
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2023-08-21 16:10:26 +0500

    Update MetaTags

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,21 @@ import Footer from '@/components/Footer'
 
 
 export const metadata = {
-  title: 'Portfolio - Muzammil',
-  description: 'Muzammil Loya\'s Portfolio',
-}
+  title: "Portfolio - Muzammil",
+  description: "Muzammil Loya's Portfolio",
+  keywords: ["Web Developer", "Full stack Developer", "Developer for hire"],
+  verification: { google: "zlT0pnlw_0XvL7gPOhY1rwmoUUDMQOKcbqyhVRNcEBc" },
+  category: "technology",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='h-screen'>
-      <body className='flex-col h-screen'>
+    <html lang="en" className="h-screen">
+      <body className="flex-col h-screen ">
         <Header />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
