d5127c44a8b4bfb32d32dacda1b56ae2681ef552
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-26 02:42:54 +0500

    refactor: Update LatestBlog component to improve UI consistency and responsiveness

--- components/home/blog/LatestBlog.tsx
+++ components/home/blog/LatestBlog.tsx
@@ -29,25 +29,25 @@ const LatestBlog = ({ post }: { post: PostType }) => {
     }
   }, [post.createdAt]);
   return (
-    <section className="px-4 py-8 w-full flex flex-col md:flex-row bg-white dark:bg-slate-900 justify-center gap-6 h-[572px]">
-      <main className="max-w-3xl w-full md:w-2/3">
-        <h1 className="text-5xl font-extrabold dark:text-white mb-6">
+    <section className="px-4 py-8 w-full flex flex-col lg:flex-row bg-white dark:bg-slate-900 justify-center gap-6 lg:h-[572px]">
+      <main className="max-w-3xl w-full lg:w-2/3">
+        <h1 className="text-3xl sm:text-5xl font-extrabold dark:text-white mb-6">
           Latest Blog Post
         </h1>
         <Link href={`blogs/${post.id}`}>
           <div className="w-full h-auto aspect-video relative">
             <div className="absolute bottom-0 left-0 m-4">
-              <p className="texl-2xl  font-bold bg-white dark:bg-slate-800 py-2 px-4 rounded-t-lg inline-block uppercase">
+              <p className="sm:texl-2xl text-xs  font-bold bg-white dark:bg-slate-800 py-2 px-4 rounded-t-lg inline-block uppercase">
                 {/* Span for purple dot */}
                 <span className="bg-indigo-700 w-2 h-2 rounded-full inline-block mr-2"></span>
                 {post.topic}
               </p>
-              <h2 className="text-4xl font-bold bg-white dark:bg-slate-800 p-4 rounded-b-lg rounded-tr-lg inline-block">
+              <h2 className="sm:text-4xl font-extrabold bg-white dark:bg-slate-800 p-2 sm:p-4 rounded-b-lg rounded-tr-lg inline-block">
                 {post.title}
               </h2>
             </div>
             <div className="absolute top-4 right-4 rounded-full bg-white dark:bg-slate-800 px-2">
-              <p className="p-2 font-bold">{dateCreated()}</p>
+              <p className="max-sm:text-xs p-2 font-bold">{dateCreated()}</p>
             </div>
             <Image
               className="w-full h-full object-cover rounded-lg"
@@ -59,37 +59,37 @@ const LatestBlog = ({ post }: { post: PostType }) => {
           </div>
         </Link>
       </main>
-      <aside className="w-1/3 h-auto">
+      <aside className="lg:w-1/3 max-lg:max-w-3xl">
         <h4 className="text-3xl font-bold mb-9">More</h4>
-        <ul className="flex flex-col h-5/6 gap-4">
+        <ul className="flex lg:flex-col h-5/6 gap-4 w-full">
           <Link
             href={`/blogs`}
-            className="relative flex h-1/2 items-center justify-center overflow-hidden rounded-lg group parent"
+            className="relative flex h-1/2 items-center justify-center overflow-hidden rounded-lg group parent max-lg:w-1/2"
           >
             <Image
               src={"/blog-background.png"}
               height={90}
               width={160}
               alt="To project page"
-              className="w-full h-auto object-contain"
+              className="w-full h-auto object-contain rounded-lg"
             />
-            <h5 className="absolute text-3xl font-bold uppercase !text-white bg-slate-800 px-4 py-2 rounded-md group-hover:text-2xl">
+            <h5 className="absolute text-xs sm:text-3xl font-bold uppercase !text-white bg-slate-800 px-4 py-2 rounded-lg sm:group-hover:text-2xl">
               More Blog Posts
             </h5>
           </Link>
 
           <Link
             href={`/projects`}
-            className="relative flex h-1/2 items-center justify-center overflow-hidden rounded-lg group"
+            className="relative flex h-1/2 items-center justify-center overflow-hidden rounded-lg group max-lg:w-1/2"
           >
             <Image
               src={"/project_background.png"}
               height={90}
               width={160}
               alt="To project page"
-              className="w-full h-auto object-contain"
+              className="w-full h-auto object-contain rounded-lg"
             />
-            <h5 className="absolute text-3xl font-bold uppercase !text-white group-hover:text-2xl">
+            <h5 className="absolute text-xs sm:text-3xl font-bold uppercase !text-white sm:group-hover:text-2xl">
               My projects
             </h5>
           </Link>
