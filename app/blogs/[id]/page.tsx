05e5c182bc64fccd38202df596945f0dc139b379
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-26 20:12:11 +0500

    refactor: Update page component to generate metadata for blog posts

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Metadata, ResolvingMetadata } from "next";
 
 import { notFound } from "next/navigation";
 
+const BASE_URL = "https://muzammilloya-portfolio.vercel.app";
+
 interface Props {
   params: { id: string };
 }
@@ -14,7 +16,17 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const { id } = params;
   const post = await getPostById(id);
-  const { creator, authors, publisher } = await parent;
+  const {
+    creator,
+    authors,
+    publisher,
+    openGraph: { images },
+    twitter: { images: twitterImages },
+  } = await parent;
+  const description = (post.title + " " + post.desc.raw.toString()).substring(
+    0,
+    130
+  );
 
   return {
     title: post.title,
@@ -23,6 +35,30 @@ export async function generateMetadata(
     publisher,
     authors,
     keywords: [post.topic],
+    description,
+    openGraph: {
+      title: post.title,
+      description,
+      url: `${BASE_URL}/blogs/${id}`,
+      type: "article",
+      locale: "en_US",
+      images: [
+        {
+          url: post.coverImage.url,
+          width: 800,
+          height: 600,
+          alt: post.title,
+        },
+        ...images,
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@MuzammilLoya",
+      title: post.title,
+      description,
+      images: [post.coverImage.url, ...twitterImages],
+    },
   };
 }
 
