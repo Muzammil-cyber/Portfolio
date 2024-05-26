51f0dbe3e1ba381e58468171987ed25a9a95692b
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-26 19:22:28 +0500

    refactor: Update NotFound component to improve UI consistency and responsiveness

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,17 @@
 // <div style="font-family:system-ui,&quot;Segoe UI&quot;,Roboto,Helvetica,Arial,sans-serif,&quot;Apple Color Emoji&quot;,&quot;Segoe UI Emoji&quot;;height:100vh;text-align:center;display:flex;flex-direction:column;align-items:center;justify-content:center"><div><style>body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}</style><h1 class="next-error-h1" style="display:inline-block;margin:0 20px 0 0;padding:0 23px 0 0;font-size:24px;font-weight:500;vertical-align:top;line-height:49px">404</h1><div style="display:inline-block"><h2 style="font-size:14px;font-weight:400;line-height:49px;margin:0">This page could not be found.</h2></div></div></div>
 
 import AnimatedText from "@/components/home/Hero/AnimateText";
-// import { getImageLocal } from "@/utils/image";
 
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function NotFound() {
-  // const { img } = await getImageLocal("/not-found.png");
   return (
     <main className="flex-auto min-h-full text-center flex items-center justify-center flex-col sm:flex-row p-10 bg-white dark:bg-slate-900">
       <Image
         src={"/not-found.png"}
-        width={100}
-        height={150}
+        width={500}
+        height={500}
         placeholder="empty"
         priority={false}
         alt="Not Found"
