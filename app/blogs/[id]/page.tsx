3a243e4a3b1a36b39c4ca381473884570bd04639
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-26 18:54:58 +0500

    refactor: Add error handling for missing blog post in page component

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { getPostById } from "@/api/req";
 import WholeBlog from "@/components/home/blog/WholeBlog";
-import RawToHtml from "@/utils/rawToHtml";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const post = await getPostById(id);
+  if (!post) return notFound();
   return (
     <main className="w-full max-w-4xl py-6 px-4 mx-auto">
       <WholeBlog post={post} />
