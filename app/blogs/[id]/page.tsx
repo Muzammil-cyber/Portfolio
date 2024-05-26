43edea49e603ea588d618c62c22de39dbb8ba4e6
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-26 19:09:14 +0500

    refactor: Update page component to handle missing blog post and improve UI consistency and responsiveness

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const post = await getPostById(id);
-  if (!post) return notFound();
+  if (!post) notFound();
   return (
     <main className="w-full max-w-4xl py-6 px-4 mx-auto">
       <WholeBlog post={post} />
