7cb83dc7ad4f81244813ec0a3dd01d2a5d22814e
Muzammil-cyber <muzammilloya@hotmail.com>
2024-06-01 14:45:38 +0500

    refactor: Update Loading component styles for better UI consistency
    Update the styles of the Loading component in the blogs directory to improve UI consistency. The changes include modifying the width and height properties to use screen dimensions and applying a dark background color and white text color. These updates enhance the visual appearance of the loading spinner and maintain a cohesive design across the application.

--- app/blogs/loading.tsx
+++ app/blogs/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex flex-grow justify-center items-center w-full h-full">
-      <div className="spinner"></div>
+    <div className="w-screen  h-screen dark:bg-slate-900 dark:text-white">
+      <div className="flex flex-grow justify-center items-center w-full h-full">
+        <div className="spinner"></div>
+      </div>
     </div>
   );
 }
