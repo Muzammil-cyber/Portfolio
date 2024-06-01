ee934c95d00a2e320fcf69718585f4d5d59fdc92
Muzammil-cyber <muzammilloya@hotmail.com>
2024-06-01 14:31:19 +0500

    refactor: Update HeroHome and LatestBlog components
    Fix image loading issues and improve UI consistency in HeroHome and LatestBlog components. Set priority prop to false in HeroHome to prevent high priority loading. Update heading tags from h4 to h3 in LatestBlog for better semantic structure. Replace ul element with div element to address accessibility issues. Enhance user experience and maintain code clarity and maintainability.

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -12,15 +12,16 @@ export default function HeroHome() {
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-snug  md:text-5xl lg:text-6xl ">
           Hi, I&apos;m <span className="text-indigo-700">Muzammil Loya</span>.
           <br />
-          Software Engineer
+          <AnimatedText text="Software Engineer" duration={2} />
         </h1>
         <h2 className="mb-8 font-bold text-lg  text-slate-500 lg:text-xl   dark:text-slate-400">
           <AnimatedText
             text="Executive Director of Web Devlopment @ GDSC"
-            duration={2}
+            duration={4}
+            delay={3}
           />
-          <br /> <AnimatedText text="Freelancer" duration={1} delay={2} />{" "}
-          <br /> <AnimatedText text="IoBM '26" duration={1} delay={3} />
+          <br /> <AnimatedText text="Freelancer" duration={2} delay={3} />
+          <br /> <AnimatedText text="IoBM '26" duration={2} delay={3} />
         </h2>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center lg:justify-end sm:space-y-0">
           <HeroLink />
