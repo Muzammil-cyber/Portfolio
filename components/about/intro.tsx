d248b6e602f08f010ce3c2a554e605af67db5bc5
Muhammad Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2024-12-01 21:38:47 +0500

    refactor: Enhance Intro component layout and update skills to include NextJS

--- components/about/intro.tsx
+++ components/about/intro.tsx
@@ -5,13 +5,13 @@ import age from "./intro/age";
 
 export default function Intro({ img, base64 }: ImageType) {
   return (
-    <section className="flex flex-col md:flex-row md:justify-center items-center gap-4 py-8">
-      <article className="sm:w-1/2 text-left space-y-4  text-lg mx-auto">
-        <h2 className="text-3xl font-bold ">Hi, I&apos;m Muzammil Loya 👋</h2>{" "}
+    <section className="flex flex-col items-center gap-4 py-8 md:flex-row md:justify-center">
+      <article className="mx-auto space-y-4 text-left text-lg sm:w-1/2">
+        <h2 className="text-3xl font-bold">Hi, I&apos;m Muzammil Loya 👋</h2>{" "}
         <p className="text-secondary-800 dark:text-secondary-400">
           I&apos;m a {age()}-year-old{" "}
           <span className="text-bold">software engineer</span> with expertise in
-          React Native, ReactJS, and Node.js. I&apos;m passionate about building
+          React Native, NextJS, and Node.js. I&apos;m passionate about building
           robust and scalable software solutions that enhance user experience.
         </p>
         <p className="text-secondary-600 dark:text-secondary-400">
@@ -20,8 +20,8 @@ export default function Intro({ img, base64 }: ImageType) {
           worldwide. 🌍
         </p>
         <p className="text-secondary-600 dark:text-secondary-400">
-          Currently, I&apos;m seeking a challenging position to broaden my
-          skillset and tackle new technical problems. I thrive in innovative
+          Currently, I&apos;m seeking a challenging position to broaden my skill
+          set and tackle new technical problems. I thrive in innovative
           environments and believe in collaborative problem-solving. &#x1F60E;
         </p>
         <p className="text-secondary-600 dark:text-secondary-400">
@@ -37,7 +37,7 @@ export default function Intro({ img, base64 }: ImageType) {
         </p>
       </article>
       <Image
-        className="rounded-full w-1/2 h-auto  max-md:mt-4 image-back "
+        className="image-back h-auto w-1/2 rounded-full max-md:mt-4"
         src={img.src}
         alt="It's a me, Muzammil"
         width={img.width}
